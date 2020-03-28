5175bd72565fc6703d5202444b4adc9e84a0e137
Mayank Rikh <mayankrikh@gmail.com>
2020-03-28 22:28:24 +0000

    Added tokens ad color to UI

--- src/main/java/org/rikh/views/PokerPane.java
+++ src/main/java/org/rikh/views/PokerPane.java
@@ -1,11 +1,12 @@
 package org.rikh.views;
 
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.scene.text.TextAlignment;
 import org.rikh.model.Hand;
 import org.rikh.utilities.Constants;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
@@ -20,10 +21,14 @@ public class PokerPane extends Pane {
     private double spacing = 10.0;
     private double defaultCardWidth = 50.0;
     private double defaultCardHeight = 80.0;
+    private double defaultCoinRadius = 10.0;
+
+    private int defaultCoinCount = 10;
 
     public PokerPane(double width, double height, String[] playerCards){
         this.width = width;
         this.height = height;
+        setStyle("-fx-background-color: green");
         initialSetup(playerCards);
     }
 
@@ -37,13 +42,18 @@ public class PokerPane extends Pane {
     //Private methods below here
     private void initialSetup(String[] playerCards){
 
+        setupPotArea();
+
         //opponent cards
-        setupCards(height/10.0, Constants.opponent, "c", null);
+        double opponentHeight = height/10.0;
+        setupCards(opponentHeight, Constants.opponent, "c", null, false);
+
         //player cards
-        setupCards(height/10.0 * 8.0, Constants.player, "p", playerCards);
+        double playerHeight = height/10.0 * 8.5;
+        setupCards(playerHeight, Constants.player, "p", playerCards, true);
     }
 
-    private void setupCards(double cardCenterY, String title, String id, String[] playerCards){
+    private void setupCards(double cardCenterY, String title, String id, String[] cards, boolean tokenAbove){
 
         double centerX = width/2.0;
         double cardWidth = Math.max(defaultCardWidth, width/14.0);
@@ -60,9 +70,9 @@ public class PokerPane extends Pane {
 
             StackPane card;
             try{
-                card = setupCard(id + i, cardWidth, cardHeight, playerCards[i]);
+                card = setupCard(id + "c" + i, cardWidth, cardHeight, cards[i]);
             }catch (NullPointerException e){
-                card = setupCard(id + i, cardWidth, cardHeight, "?");
+                card = setupCard(id + "c" + i, cardWidth, cardHeight, "?");
             }
             cardPane.getChildren().add(card);
         }
@@ -70,7 +80,53 @@ public class PokerPane extends Pane {
         cardPane.setLayoutX(cardsStartX);
         cardPane.setLayoutY(cardStartY);
 
-        getChildren().addAll(cardPane);
+        getChildren().add(cardPane);
+        setupTokens(tokenAbove ? cardStartY : cardStartY + cardHeight, id, tokenAbove);
+    }
+
+    private void setupPotArea(){
+
+        Circle circle = new Circle();
+        circle.setRadius(width/6.0);
+        circle.setCenterX(width/2.0);
+        circle.setCenterY(height/2.0);
+        circle.setStrokeWidth(3.0);
+        circle.setStroke(Paint.valueOf("#ffffff"));
+        circle.setFill(Paint.valueOf("#00ff00"));
+        getChildren().add(circle);
+
+        Line line = new Line();
+        line.setStartX(0);
+        line.setStrokeWidth(3.0);
+        line.setEndX(width);
+        line.setStartY(height/2.0);
+        line.setEndY(height/2.0);
+        line.setStroke(Paint.valueOf("#ffffff"));
+        getChildren().add(line);
+    }
+
+    private void setupTokens(double height, String id, boolean tokenAbove){
+
+        FlowPane tokenPane = new FlowPane();
+        tokenPane.setPrefWrapLength(defaultCoinCount * (spacing +  2 * defaultCoinRadius));
+        tokenPane.setHgap(spacing);
+
+        for (int i = 0; i < defaultCoinCount; i++){
+            Circle circle = new Circle();
+            circle.setRadius(defaultCoinRadius);
+            circle.setId(id + "t" + i);
+            circle.setStroke(Paint.valueOf("#000000"));
+            circle.setFill(Paint.valueOf("#fdd023"));
+            tokenPane.getChildren().add(circle);
+        }
+
+        double tokenX = width/2.0 - (defaultCoinCount/2.0 * (spacing + 2 * defaultCoinRadius));
+        double tokenY = tokenAbove ? (height - 10.0 - (2 * defaultCoinRadius)) : height + 10.0;
+
+        tokenPane.setLayoutX(tokenX);
+        tokenPane.setLayoutY(tokenY);
+
+        getChildren().add(tokenPane);
     }
 
     private StackPane setupCard(String id, double width, double height, String title){
